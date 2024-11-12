dc9d1c75aaaf3ebd8d7e733144c86c779b218c98
RudenkoAndreyKh <rudenkoandreykh@gmail.com>
2024-11-12 12:17:13 +0200

    trying to limit vram using

--- plant_recognition/recognition_train.py
+++ plant_recognition/recognition_train.py
@@ -14,6 +14,17 @@ if physical_devices:
 else:
     print("GPU не знайдено. Використовується CPU.")
 
+# Вивести доступні GPU 
+gpus = tf.config.list_physical_devices('GPU') 
+if gpus: 
+    try: # Встановлення обмеження пам'яті GPU 
+        for gpu in gpus: 
+            tf.config.experimental.set_memory_growth(gpu, True) 
+        logical_gpus = tf.config.list_logical_devices('GPU') 
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs") 
+    except RuntimeError as e: 
+        print(e)
+
 warnings.filterwarnings('ignore')
 
 def recognition_train():
